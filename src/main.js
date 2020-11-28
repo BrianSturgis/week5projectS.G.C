2eda255d82af978a6420992d621c2af9a4749a98
Brian Sturgis <sturujisu@gmail.com>
2020-11-27 22:36:40 -0800

    first commit to start project

---
+++ src/main.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import Triangle from './triangle.js';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // $('#triangle-checker-form').submit(function(event) {
+  //   event.preventDefault();
+  //   const length1 = $('#length1').val();
+    
+  //   const response = triangle.checkType();
+  //   // $('#response').append("<p>" + response + "</p>");
+  // });
