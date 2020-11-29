2f8b2065ed951eabd30208aacf88ecb724a75299
Brian Sturgis <sturujisu@gmail.com>
2020-11-28 17:48:27 -0800

    add a user input varriable of 70 for testing

--- src/triangle.js
+++ src/triangle.js
@@ -1,16 +1,16 @@
 export default class Earthage {
   constructor() {
-    this.input = 0;
+    this.input = userInput;
     this.alienYears = [];
     this.lifeexp = [];
     this.pastLife = 0;
     this.lifeLeft = 0;
   }
   alienAge(){
-    // let array = [0.24, 0.62, 1.88, 11.86];
-    // for(let i = 0; i < array.length; i++) {
-    //   this.alienYears.push(Math.round(userInput / array[i]));
-    // }
+    let array = [0.24, 0.62, 1.88, 11.86];
+    for(let i = 0; i < array.length; i++) {
+      this.alienYears.push(Math.round(userInput / array[i]));
+    }
   }
 
 }
@@ -21,3 +21,11 @@ export default class Earthage {
 
 
 
+let userInput = 70;
+
+// let joe = new Earthage();
+// joe.ageCheck();
+// joe.alienAge();
+// // joe.alienExpectancy();
+
+// console.log(joe);
