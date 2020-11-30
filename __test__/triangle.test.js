4cbbf020a34f1dabfc5694847b980c7a6ce7698d
Brian Sturgis <sturujisu@gmail.com>
2020-11-29 17:59:30 -0800

    be a good pass for alienage function at [0]

--- __test__/triangle.test.js
+++ __test__/triangle.test.js
@@ -1,20 +1,32 @@
-import { TestScheduler } from 'jest';
-import EarthAge from '../src/triangle.js';
+
+
+import EarthAge from '../js/triangle.js';
 
 describe('EarthAge', () => {
+
+  let earthAge;
+
+    beforeEach(() => {
+      earthAge = new EarthAge(70,[],[],0,0);
+      earthAge.alienAge();
+
+  });
+  
+  test('should correctly create a triangle object with three lengths', () => {
+    expect(earthAge.value).toEqual(70);
+    expect(earthAge.pastLife).toEqual(0);
+    expect(earthAge.lifeLeft).toEqual(0);
+});
+  test('should correctly determine whether three lengths are not a triangle', () => {
+    expect(earthAge.alienYears[0]).toEqual(292);
+});
+  test('should correctly determine whether three lengths are not a triangle', () => {
+    expect(earthAge.alienYears[1]).toEqual(113);
+  });
+
   
 
-  test('should correctly create a Earthage object five key values', () => {
-    const age = new EarthAge(70,[],[],0,0);
-    expect(age.input).toEqual(70);
-    expect(age.alienYears).toEqual([]);
-    expect(age.lifeexp).toEqual([]);
-    expect(age.pastLife).toEqual(0);
-    expect(age.lifeLeft).toEqual(0);
+  test('should correctly determine whether three lengths are not a triangle', () => {
+    expect(earthAge.life[2]).toEqual(2);
   });
 });
-  test('shoulde create an array of input / array data', () => {
-    const alien = new EarthAge
-      expect(alien.alienAge()).toEqual();
-  });
-    
---
+++ js/triangle.js
@@ -0,0 +1,30 @@
+export default class EarthAge {
+    constructor() {
+      this.value = userInput;
+      this.alienYears = [];
+      this.life = [];
+      this.pastLife = 0;
+      this.lifeLeft = 0;
+    }
+    alienAge(){
+      let array = [0.24, 0.62, 1.88, 11.86];
+      for(let i = 0; i < array.length; i++) {
+        this.alienYears.push(Math.round(userInput / array[i]));
+      }
+    }
+}
+
+
+
+
+
+
+
+let userInput = 70;
+
+// let joe = new Earthage();
+// // // joe.ageCheck();
+// joe.alienAge();
+// // joe.alienExpectancy();
+
+// console.log(joe);
