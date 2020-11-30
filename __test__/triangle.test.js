4885e79fe4fe86097a176e6d0e09c09f628de33a
Brian Sturgis <sturujisu@gmail.com>
2020-11-29 18:02:38 -0800

    refactor for alienage function at [0]

--- __test__/triangle.test.js
+++ __test__/triangle.test.js
@@ -12,19 +12,19 @@ describe('EarthAge', () => {
 
   });
   
-  test('should correctly create a triangle object with three lengths', () => {
+  test('Should create and instance of EarthAge', () => {
     expect(earthAge.value).toEqual(70);
     expect(earthAge.pastLife).toEqual(0);
     expect(earthAge.lifeLeft).toEqual(0);
 });
-  test('should correctly determine whether three lengths are not a triangle', () => {
+  test('should calculate 70 years into Mercury years', () => {
     expect(earthAge.alienYears[0]).toEqual(292);
 });
   test('should correctly determine whether three lengths are not a triangle', () => {
     expect(earthAge.alienYears[1]).toEqual(113);
   });
 
-  
+
 
   test('should correctly determine whether three lengths are not a triangle', () => {
     expect(earthAge.life[2]).toEqual(2);
