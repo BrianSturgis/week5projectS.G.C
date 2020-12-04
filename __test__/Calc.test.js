c35796be42fd3be5c371358932853a2d0622420c
Brian Sturgis <sturujisu@gmail.com>
2020-12-03 22:11:58 -0800

    good pass and refactor for alienage [2]

--- __test__/Calc.test.js
+++ __test__/Calc.test.js
@@ -29,9 +29,9 @@ describe('EarthAge', () => {
   test('should calculate 70 years into jupiter years',() => {
       expect(earthAge.alienYears[3]).toEqual(6);
 });
-//   test('should calculate 70 years into years left to live in Mercury years',() => {
-//       expect(earthAge.life[3]).toEqual(4);
-// });
+  test('should calculate 70 years into years left to live in Mercury years',() => {
+      expect(earthAge.life[3]).toEqual(4);
+});
 //   test('should calculate 70 years into years left to live in Venus years',() => {
 //       expect(earthAge.life[2]).toEqual(27);
 // });
