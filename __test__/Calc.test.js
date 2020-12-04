b972e4f4b95a8570586de443099b57a9080b382a
Brian Sturgis <sturujisu@gmail.com>
2020-12-03 22:09:11 -0800

    good fail for alienage [1]

--- __test__/Calc.test.js
+++ __test__/Calc.test.js
@@ -20,9 +20,9 @@ describe('EarthAge', () => {
   test('should calculate 70 years into Mercury years', () => {
       expect(earthAge.alienYears[0]).toEqual(292);
 });
-//   test('should calculate 70 years into Venus years', () => {
-//       expect(earthAge.alienYears[1]).toEqual(113);
-// });
+  test('should calculate 70 years into Venus years', () => {
+      expect(earthAge.alienYears[1]).toEqual(113);
+});
 //   test('should calculate 70 years into Mars years', () => {
 //       expect(earthAge.alienYears[2]).toEqual(37);
 // });
