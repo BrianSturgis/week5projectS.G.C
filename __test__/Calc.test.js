27c09108e1a86a9a29099a2281c386e40baf5078
Brian Sturgis <sturujisu@gmail.com>
2020-12-04 00:31:04 -0800

    good pass for ageCheck

--- __test__/Calc.test.js
+++ __test__/Calc.test.js
@@ -3,45 +3,48 @@ import EarthAge from './../src/js/calc.js';
 describe('EarthAge', () => {
 
     let earthAge;
-    
-
 
     beforeEach(() => {
-      earthAge = new EarthAge(70,0,0);
-      earthAge.alienAge();
-      // earthAge.alienExpectancy();
+      earthAge = new EarthAge(70,3,3);
       // earthAge.ageCheck();
+      earthAge.alienAge();
+      // earthAge.lifeExpectancy();
+      earthAge.ageCheck();
 });
     test('Should create and instance of EarthAge', () => {
       expect(earthAge.age).toEqual(70); 
-      expect(earthAge.pastLife).toEqual(0);
-      expect(earthAge.lifeLeft).toEqual(0);
+      expect(earthAge.pastLife).toEqual(3);
+      expect(earthAge.lifeLeft).toEqual(3);
   });
-  test('should calculate 70 years into Mercury years', () => {
+    test('should calculate 70 years into Mercury years', () => {
       expect(earthAge.alienYears[0]).toEqual(292);
-});
-  test('should calculate 70 years into Venus years', () => {
-      expect(earthAge.alienYears[1]).toEqual(113);
-});
-  test('should calculate 70 years into Mars years', () => {
-      expect(earthAge.alienYears[2]).toEqual(37);
-});
-  test('should calculate 70 years into jupiter years',() => {
-      expect(earthAge.alienYears[3]).toEqual(6);
-});
+  });
+    test('should calculate 70 years into Venus years', () => {
+        expect(earthAge.alienYears[1]).toEqual(113);
+  });
+    test('should calculate 70 years into Mars years', () => {
+        expect(earthAge.alienYears[2]).toEqual(37);
+  });
+    test('should calculate 70 years into jupiter years',() => {
+        expect(earthAge.alienYears[3]).toEqual(6);
+  });
 
+    test('should calculate 70 years into years left to live in Mercury years',() => {
+        expect(earthAge.lifeLeft).toEqual(3);
+  });
   test('should calculate 70 years into years left to live in Mercury years',() => {
-      expect(earthAge.life[3]).toEqual(4);
+    expect(earthAge.lifeLeft).toEqual(3);
+    expect(earthAge.pastLife).toEqual(0);
 });
-//   test('should calculate 70 years into years left to live in Venus years',() => {
-//       expect(earthAge.life[2]).toEqual(27);
-// });
-//   test('should calculate 70 years into years left to live in Mars years', () => {
-//       expect(earthAge.life[1]).toEqual(81);
-// });
-//   test('should calculate 70 years into years left to live in Mars years',() => {
-//       expect(earthAge.life[0]).toEqual(208);
-// });
+  //   test('should calculate 70 years into years left to live in Venus years',() => {
+  //       expect(earthAge.life[2]).toEqual(0);
+  // });
+  //   test('should calculate 70 years into years left to live in Mars years', () => {
+  //       expect(earthAge.life[1]).toEqual(0);
+  // });
+  //   test('should calculate 70 years into years left to live in Mars years',() => {
+  //       expect(earthAge.life[0]).toEqual(0);
+  // });
 //   // test('should create the difference of 70 vs 73',()  => 
 //   // expect(this.lifeLeft).toEqual(03);
       
--- src/js/calc.js
+++ src/js/calc.js
@@ -1,34 +1,35 @@
 // eslint disable
 export default class EarthAge {
-constructor(userInput) {
-  this.age = 70;
-  this.pastLife = 0;
-  this.lifeLeft = 0;
-  this.alienYears = [];
-  this.life = [];
-}
+  constructor(userInput, pastLife,lifeLeft) {
+    this.age = userInput;
+    this.pastLife = pastLife;
+    this.lifeLeft = lifeLeft;
+    this.alienYears = [];
+    this.life = [];
+  }
+
   alienAge(){
-  let array = [0.24, 0.62, 1.88, 11.86];
-  for(let i = 0; i < array.length; i++) {
-    this.alienYears.push(Math.round(this.age / array[i])); 
+    let array = [0.24, 0.62, 1.88, 11.86];
+    for(let i = 0; i < array.length; i++) {
+      this.alienYears.push(Math.round(this.age  / array[i]));
+    }
   }
-  }
+  ageCheck(){
+    let age;
+    if (this.age == 73 || this.age < 73) {
+      age = 73 - this.age;
+      this.lifeLeft = age;
+    }else if (this.age > 73) {
+      age = this.age - 73;
+      this.pastLife = age;
+    }
+    }
+
   // alienExpectancy(){
-  // let array = [0.24, 0.62, 1.88, 11.86];
-  // for(let i = 0; i < array.length; i++) {
-  //   this.life.push(Math.round(this.pastLife / array[i] || this.lifeLeft / array[i]));
+  //   let array = [0.24, 0.62, 1.88, 11.86];
+  //   for(let i = 0; i < array.length; i++) {
+  //     this.life.push(Math.round(this.pastLife / array[i] || this.lifeLeft / array[i]));
+  //   }
   // }
-  // }
-  // ageCheck(){
-  // let ageLeft;
-  // let agePast;
-  // if (userInput == 73 ||userInput < 73) {
-  //   ageLeft = 73 -userInput; 
-  //   this.lifeLeft = ageLeft;
-  // }else if (userInput > 73) {
-  //   agePast = userInput - 73;
-  //   this.pastLife = agePast;
-  // }
-  // }
-}
   
+  } 
